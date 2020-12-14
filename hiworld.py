a8aeda4319b29d7d19baf63735886527d176065b
lecamole <lecamole@gmail.com>
2020-12-13 23:04:16 -0700

    agregué un print

---
+++ hiworld.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 13 22:23:13 2020
+
+@author: lecamole
+"""
+
+print('hi world!')
+print('did it work?')
+print('it did!')
+print('a commit')
